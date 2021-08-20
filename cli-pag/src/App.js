936a582db0694b8c7e6b65431bc0af80f4da211d
AntonChanin <55087091+AntonChanin@users.noreply.github.com>
2021-08-20 11:07:57 +0300

    Append client pagination table page

---
+++ cli-pag/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { DataTable } from './components/dataTable/dataTable';
+
+function App() {
+  return (
+    <div className="App container">
+      <DataTable />
+    </div>
+  );
+}
+
+export default App;
